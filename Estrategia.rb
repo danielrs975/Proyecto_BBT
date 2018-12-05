ed10df0efdb5a889e9fa254095aa4f4c3f842aff
Daniel <danielrs9705@gmail.com>
2018-12-05 17:20:24 -0400

    Se agrega el archivo para implementar la clase Estrategia

---
+++ Estrategia.rb
@@ -0,0 +1,8 @@
+=begin
+En este archivo se implementa la clase Estrategia, esta clase representa a los 
+jugadores participantes tendra como subclases:
+    - Manual    ---> Aqui el usuario participa directamente en el juego, es decir,
+    escoge la jugada que quiera hacer
+    - Uniforme  ---> En esta subclase 
+
+=end 
