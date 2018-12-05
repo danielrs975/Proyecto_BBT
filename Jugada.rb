cc3b16a01d7d38c0ca10317820372582884ef393
Daniel <danielrs9705@gmail.com>
2018-12-05 17:16:04 -0400

    Se agrega el archivo para implementar la clase Jugada

---
+++ Jugada.rb
@@ -0,0 +1,12 @@
+=begin
+En este archivo se implementa la clase Jugada, esta clase representa 
+las jugadas hechas por cada uno de los jugadores en las rondas. Tendra como
+subclases:
+    - Piedra    - Papel     - Lagarto 
+    - Spock     - Tijera
+
+Y los metodos son solo dos:
+    - to_s ---> que retorna el String human-readable para mostrar por pantalla 
+    - puntos(j) ---> Tupla que representa la ganacia de puntos entre el invocante
+    y el contricante
+=end 
