0bc302b9ae495b17661495b425f4a0d1f07a8714
yuniquintero <yuni.quintero@gmail.com>
2018-12-21 00:04:26 -0400

    se empienza rondas

--- Partida.rb
+++ Partida.rb
@@ -28,18 +28,36 @@ class Partida
 
     attr_reader :mapa_partida, :nombres, :estrategias 
     
-    def initialize(mapa_partida)
+    def initialize(mapa)
 
         # Verificar que hay exactamente dos jugadores y que 
         # en efecto se trata de estrategias
         temp = []
-        mapa_partida.each_value {|value| temp.push(value) }
+        mapa.each_value {|value| temp.push(value) }
         temp1 = temp[0].instance_of?(Estrategia)
         temp2 = temp[1].instance_of?(Estrategia)
-        if mapa_partida.length == 2
-            if temp1 && temp2 
+        keys = []
+        mapa.each_key {|key| keys.push(key)}
+        if mapa.length == 2
+            if temp1 && temp2
+                $mapa_partida = mapa
+                $estrategias = temp
+                $nombres = keys
             else
                 return "Debe ingresar estrategias"
             end
         else
             return "El número de jugadores deben ser exactamente 2."
+        end
+    end
+
+    def rondas(n)
+        jugador0 = estrategias[0]
+        jugador1 = estrategias[1]
+        for i in 0..n
+            if jugador0.instance_of?(Copiar) && i > 0
+            elsif jugador1.instance_of?(Copiar) && i > 0
+            end
+        end
+    end
+end
